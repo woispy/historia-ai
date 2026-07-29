0b5b97e4d57ac75dcbfb4b3755680d97656a1203
woispy <vlknispir@gmail.com>
2026-07-29 19:02:17 +0300

    refactor: reorganize component structure

--- src/pages/Game.jsx
+++ src/pages/Game.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 
 import Layout from "../components/Layout";
 import TopBar from "../components/TopBar/TopBar";
-import MapView from "../components/MapView";
+import MapView from "../components/MapView/MapView";
 import LeftOverlay from "../components/LeftOverlay";
 import RightOverlay from "../components/RightOverlay";
 
