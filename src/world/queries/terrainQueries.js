b91b70ed078600d33575f6e8e58a13f0b1c65436
woispy <vlknispir@gmail.com>
2026-08-02 15:40:06 +0300

    Remove legacy world queries

--- src/world/queries/terrainQueries.js
+++
@@ -1,9 +0,0 @@
-export function getTerrain(gameState, terrainId) {
-  return gameState.world.map.terrain.byId[terrainId] ?? null;
-}
-
-export function getTerrains(gameState) {
-  return gameState.world.map.terrain.allIds.map(
-    (id) => gameState.world.map.terrain.byId[id]
-  );
-}
