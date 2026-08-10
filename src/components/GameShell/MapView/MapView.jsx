200436b134381e0581738f3618c2a66696f63a28
woispy <vlknispir@gmail.com>
2026-08-10 21:32:15 +0300

    Pass graphics settings into map engine

--- src/components/GameShell/MapView/MapView.jsx
+++ src/components/GameShell/MapView/MapView.jsx
@@ -2,23 +2,19 @@ import "./MapView.css";
 
 import { WorldMap } from "../../../map";
 
-function MapView({
-  gameSession,
-}) {
+function MapView({ gameSession, settings }) {
   return (
     <main className="map-view">
       <div className="map-layer terrain">
         <WorldMap
           runtime={gameSession}
+          settings={settings}
         />
       </div>
 
       <div className="country-layer" />
-
       <div className="city-layer" />
-
       <div className="army-layer" />
-
       <div className="effect-layer" />
     </main>
   );
