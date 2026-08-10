5063e8942f0b0b7544d2e5d51e7688327ac8480c
woispy <vlknispir@gmail.com>
2026-08-10 23:13:55 +0300

    Bind camera input to map viewport

--- src/map/camera/CameraViewport.jsx
+++ src/map/camera/CameraViewport.jsx
@@ -1,11 +1,27 @@
-function CameraViewport({ children }) {
+import { useEffect } from "react";
+
+function CameraViewport({ children, cameraInput = null }) {
+  useEffect(
+    () => () => cameraInput?.dispose?.(),
+    [cameraInput],
+  );
+
   return (
     <div
+      className="camera-viewport"
+      onWheel={cameraInput?.onWheel}
+      onPointerDown={cameraInput?.onPointerDown}
+      onPointerMove={cameraInput?.onPointerMove}
+      onPointerUp={cameraInput?.onPointerUp}
+      onPointerCancel={cameraInput?.onPointerCancel}
+      onClickCapture={cameraInput?.onClickCapture}
       style={{
         width: "100%",
         height: "100%",
         overflow: "hidden",
         position: "relative",
+        touchAction: "none",
+        cursor: "grab",
       }}
     >
       {children}
