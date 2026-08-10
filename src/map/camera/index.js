79d5777e14ad6ac969fb2cfb769c5fb505c66081
woispy <vlknispir@gmail.com>
2026-08-10 19:51:03 +0300

    Export camera context hook separately

--- src/map/camera/index.js
+++ src/map/camera/index.js
@@ -1,62 +1,9 @@
-/* ============================================================================
- * Camera Model
- * ========================================================================== */
-
-export {
-  createCameraModel,
-} from "./CameraModel";
-
-/* ============================================================================
- * Camera Repository
- * ========================================================================== */
-
-export {
-  createCameraRepository,
-} from "./CameraRepository";
-
-/* ============================================================================
- * Camera Queries
- * ========================================================================== */
-
-export {
-  getCamera,
-  getCameraPosition,
-  getCameraZoom,
-  getCameraTarget,
-} from "./CameraQueries";
-
-/* ============================================================================
- * Camera Actions
- * ========================================================================== */
-
-export {
-  moveCamera,
-  zoomCamera,
-  resetCamera,
-  focusCamera,
-} from "./CameraActions";
-
-/* ============================================================================
- * Camera Bootstrap
- * ========================================================================== */
-
-export {
-  bootstrapCamera,
-} from "./CameraBootstrap";
-
-/* ============================================================================
- * React Hooks
- * ========================================================================== */
-
-export {
-  useCamera,
-  useCameraFocus,
-} from "./hooks";
-
-/* ============================================================================
- * Camera Viewport
- * ========================================================================== */
-
+export { createCameraModel } from "./CameraModel";
+export { createCameraRepository } from "./CameraRepository";
+export { getCamera, getCameraPosition, getCameraZoom, getCameraTarget } from "./CameraQueries";
+export { moveCamera, zoomCamera, resetCamera, focusCamera } from "./CameraActions";
+export { bootstrapCamera } from "./CameraBootstrap";
+export { useCamera, useCameraFocus } from "./hooks";
 export {
   createViewportModel,
   resizeViewport,
@@ -65,28 +12,8 @@ export {
   getViewportCenter,
   useViewport,
 } from "./viewport";
-
-/* ============================================================================
- * React Components
- * ========================================================================== */
-
-export {
-  useCameraContext,
-  default as CameraProvider,
-} from "./CameraProvider";
-
-export {
-  default as CameraController,
-} from "./CameraController";
-
-export {
-  default as CameraViewport,
-} from "./CameraViewport";
-
-/* ============================================================================
- * Camera Services
- * ========================================================================== */
-
-export {
-  getProvinceFocus,
-} from "./services";
+export { useCameraContext } from "./CameraContext";
+export { default as CameraProvider } from "./CameraProvider";
+export { default as CameraController } from "./CameraController";
+export { default as CameraViewport } from "./CameraViewport";
+export { getProvinceFocus } from "./services";
