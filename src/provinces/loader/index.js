e4efe9571ed98a4360da7894202116c6893e955b
woispy <vlknispir@gmail.com>
2026-08-10 23:24:44 +0300

    Expose historical province manifest loader

--- src/provinces/loader/index.js
+++ src/provinces/loader/index.js
@@ -1,15 +1,5 @@
-export {
-  loadProvinceManifest,
-} from "./ProvinceManifestLoader.js";
-
-export {
-  loadProvinceAssets,
-} from "./ProvinceAssetLoader.js";
-
-export {
-  loadProvinceRepository,
-} from "./ProvinceRepositoryLoader.js";
-
-export {
-  loadHistoricalProvinceRepository,
-} from "./HistoricalProvinceRepositoryLoader.js";
+export { loadProvinceManifest } from "./ProvinceManifestLoader.js";
+export { loadProvinceAssets } from "./ProvinceAssetLoader.js";
+export { loadProvinceRepository } from "./ProvinceRepositoryLoader.js";
+export { loadHistoricalProvinceManifest } from "./HistoricalProvinceManifestLoader.js";
+export { loadHistoricalProvinceRepository } from "./HistoricalProvinceRepositoryLoader.js";
