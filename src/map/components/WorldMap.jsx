0d2713ac163d55691f2cd498ee782e531774ddb6
woispy <vlknispir@gmail.com>
2026-08-10 21:29:53 +0300

    Use continuous camera world wrapping in map renderer

--- src/map/components/WorldMap.jsx
+++ src/map/components/WorldMap.jsx
@@ -6,25 +6,20 @@ import { RenderRoot, RenderLayer, SvgRenderer } from "../rendering";
 function WorldMap({ runtime, selectedProvinceId, onProvinceClick }) {
   const { provinces } = useWorldMap(runtime);
   const camera = useCamera();
-  const worldCopies = [-720, -360, 0, 360, 720];
 
   return (
     <CameraProvider value={camera}>
       <CameraController zoom={camera.zoom} move={camera.move} />
-      <CameraViewport camera={camera.camera}>
+      <CameraViewport>
         <RenderRoot>
-          <SvgRenderer>
-            {worldCopies.map((offset) => (
-              <g key={offset} transform={`translate(${offset} 0)`}>
-                <RenderLayer>
-                  <ProvinceLayer
-                    provinces={provinces}
-                    selectedProvinceId={selectedProvinceId}
-                    onProvinceClick={onProvinceClick}
-                  />
-                </RenderLayer>
-              </g>
-            ))}
+          <SvgRenderer camera={camera.camera}>
+            <RenderLayer>
+              <ProvinceLayer
+                provinces={provinces}
+                selectedProvinceId={selectedProvinceId}
+                onProvinceClick={onProvinceClick}
+              />
+            </RenderLayer>
           </SvgRenderer>
         </RenderRoot>
       </CameraViewport>
