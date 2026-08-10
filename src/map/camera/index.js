3303f303a8ce9ca0e3bdb41ca0c4e7df19353001
woispy <vlknispir@gmail.com>
2026-08-10 23:14:10 +0300

    Expose optimized camera controller API

--- src/map/camera/index.js
+++ src/map/camera/index.js
@@ -1,7 +1,14 @@
 export { createCameraModel } from "./CameraModel";
 export { createCameraRepository } from "./CameraRepository";
 export { getCamera, getCameraPosition, getCameraZoom, getCameraTarget } from "./CameraQueries";
-export { moveCamera, zoomCamera, resetCamera, focusCamera } from "./CameraActions";
+export {
+  moveCamera,
+  zoomCamera,
+  setCameraZoom,
+  setCameraPosition,
+  resetCamera,
+  focusCamera,
+} from "./CameraActions";
 export { bootstrapCamera } from "./CameraBootstrap";
 export { useCamera, useCameraFocus } from "./hooks";
 export {
@@ -14,6 +21,6 @@ export {
 } from "./viewport";
 export { useCameraContext } from "./CameraContext";
 export { default as CameraProvider } from "./CameraProvider";
-export { default as CameraController } from "./CameraController";
+export { useCameraController } from "./CameraController";
 export { default as CameraViewport } from "./CameraViewport";
 export { getProvinceFocus } from "./services";
