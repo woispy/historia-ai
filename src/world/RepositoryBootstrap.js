c0177297a2da14a3aa924b4c85b62f6a18a86174
woispy <vlknispir@gmail.com>
2026-08-10 23:16:57 +0300

    Bootstrap provinces from historical registry

--- src/world/RepositoryBootstrap.js
+++ src/world/RepositoryBootstrap.js
@@ -1,29 +1,41 @@
 import { createCountryRepositoryFromArray } from "../countries";
 import { createCityRepositoryFromArray } from "../cities";
-import { bootstrapProvinces } from "../provinces";
-import { applyHistoricalProvinceOwnership } from "../provinces/HistoricalProvinceOwnership.js";
+import { loadHistoricalProvinceRepository } from "../provinces";
 import { createCharacterRepository } from "../characters";
 import { createFamilyRepository } from "../family";
 import { createKnowledgeRepository } from "../knowledge";
 import { createSelectionRepository } from "../selection";
 
+function getHistoricalRegistry(scenario) {
+  return Object.values(scenario.data.historical ?? {})[0] ?? null;
+}
+
 export function createRepositories(scenario) {
   if (!scenario) throw new Error("Scenario is required.");
 
-  const historicalRegistry = Object.values(scenario.data.historical ?? {})[0] ?? null;
+  const historicalRegistry = getHistoricalRegistry(scenario);
   const scenarioCountries = Object.values(scenario.data.countries ?? {});
   const historicalCountries = Object.values(historicalRegistry?.countries ?? {});
   const countriesById = {};
 
-  for (const country of historicalCountries) countriesById[country.id] = country;
-  for (const country of scenarioCountries) {
-    countriesById[country.id] = { ...(countriesById[country.id] ?? {}), ...country };
+  for (const country of historicalCountries) {
+    countriesById[country.id] = country;
   }
+
+  for (const country of scenarioCountries) {
+    countriesById[country.id] = {
+      ...(countriesById[country.id] ?? {}),
+      ...country,
+    };
+  }
+
+  const provinces = loadHistoricalProvinceRepository(historicalRegistry);
 
   return {
     countries: createCountryRepositoryFromArray(Object.values(countriesById)),
     cities: createCityRepositoryFromArray(Object.values(scenario.data.cities ?? {})),
-    provinces: applyHistoricalProvinceOwnership(bootstrapProvinces(), historicalRegistry),
+    provinces,
+    historicalProvinces: provinces,
     characters: createCharacterRepository(),
     families: createFamilyRepository(),
     knowledge: createKnowledgeRepository(),
