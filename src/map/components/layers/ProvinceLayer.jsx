38b23ef671ae2908583630efaf00f43abbaad9b6
woispy <vlknispir@gmail.com>
2026-08-11 20:20:52 +0300

    Clip political provinces to physical water boundaries

--- src/map/components/layers/ProvinceLayer.jsx
+++ src/map/components/layers/ProvinceLayer.jsx
@@ -2,23 +2,34 @@ import ProvinceSvg from "../ProvinceSvg";
 import ProvincePolygon from "../ProvincePolygon";
 import { ANATOLIA_PHYSICAL_ATLAS } from "../../data/AnatoliaPhysicalAtlas";
 
-function buildMaskPath(polygons) {
+function buildPathData(polygons = []) {
   return polygons
+    .filter((polygon) => Array.isArray(polygon) && polygon.length >= 3)
     .map((polygon) => {
       const [first, ...rest] = polygon;
-      return [`M ${first[0]} ${first[1]}`, ...rest.map(([x, y]) => `L ${x} ${y}`), "Z"].join(" ");
+      return [
+        `M ${first[0]} ${first[1]}`,
+        ...rest.map(([x, y]) => `L ${x} ${y}`),
+        "Z",
+      ].join(" ");
     })
     .join(" ");
 }
 
 function ProvinceLayer({ provinces, selectedProvinceId, onProvinceClick, mapStyle, mapShadows }) {
-  const landMaskPath = buildMaskPath(ANATOLIA_PHYSICAL_ATLAS.landPolygons);
+  const landPath = buildPathData(ANATOLIA_PHYSICAL_ATLAS.landPolygons);
+  const waterHolesPath = buildPathData(
+    ANATOLIA_PHYSICAL_ATLAS.seas.map((sea) => sea.coordinates),
+  );
 
   return (
     <ProvinceSvg>
       <defs>
         <clipPath id="anatolia-landmask" clipPathUnits="userSpaceOnUse">
-          <path d={landMaskPath} fillRule="evenodd" />
+          <path
+            d={`${landPath} ${waterHolesPath}`}
+            fillRule="evenodd"
+          />
         </clipPath>
       </defs>
       {provinces.map(({ province, country, geometry }) => (
