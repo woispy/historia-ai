c44347ddca101bd8c45bd6a71c6a5c6accadb059
woispy <vlknispir@gmail.com>
2026-08-13 19:07:44 +0300

    refactor map coastline overlay

--- src/map/components/layers/WorldPhysicalLayer.jsx
+++ src/map/components/layers/WorldPhysicalLayer.jsx
@@ -5,38 +5,14 @@ import {
 
 function WorldPhysicalLayer() {
   return (
-    <g aria-label="Global physical geography">
-      <defs>
-        <clipPath id="world-landmask" clipPathUnits="userSpaceOnUse">
-          <path d={WORLD_LAND_PATH} fillRule="nonzero" />
-        </clipPath>
-      </defs>
-
-      <rect
-        x={WORLD_PHYSICAL_ATLAS.bounds.minX}
-        y={WORLD_PHYSICAL_ATLAS.bounds.minY}
-        width={360}
-        height={180}
-        fill={WORLD_PHYSICAL_ATLAS.water.fill}
-        pointerEvents="none"
-      />
-
-      <path
-        d={WORLD_LAND_PATH}
-        fill={WORLD_PHYSICAL_ATLAS.land.baseFill}
-        stroke="none"
-        fillRule="nonzero"
-        pointerEvents="none"
-      />
-
+    <g aria-label="Global coastline authority" pointerEvents="none">
       <path
         d={WORLD_LAND_PATH}
         fill="none"
         stroke={WORLD_PHYSICAL_ATLAS.water.coastline}
         strokeWidth="0.10"
-        strokeOpacity="0.70"
+        strokeOpacity="0.72"
         vectorEffect="non-scaling-stroke"
-        pointerEvents="none"
       />
     </g>
   );
