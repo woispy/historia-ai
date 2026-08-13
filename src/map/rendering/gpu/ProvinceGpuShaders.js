bb5d4699772e2a72b61a2324c7a6afefdf7e9a3a
woispy <vlknispir@gmail.com>
2026-08-13 19:05:28 +0300

    feat(map): add GPU province shaders

---
+++ src/map/rendering/gpu/ProvinceGpuShaders.js
@@ -0,0 +1,58 @@
+export const PROVINCE_VERTEX_SHADER = `#version 300 es
+precision highp float;
+
+in vec2 aPosition;
+in vec2 aUv;
+
+uniform vec2 uCameraCenter;
+uniform vec2 uViewSize;
+uniform vec2 uViewportScale;
+
+out vec2 vUv;
+
+void main() {
+  vec2 normalized = (aPosition - uCameraCenter) / (uViewSize * 0.5);
+  normalized *= uViewportScale;
+  gl_Position = vec4(normalized, 0.0, 1.0);
+  vUv = aUv;
+}
+`;
+
+export const PROVINCE_FRAGMENT_SHADER = `#version 300 es
+precision highp float;
+
+uniform sampler2D uProvinceIds;
+uniform sampler2D uLandMask;
+uniform sampler2D uPalette;
+uniform float uPaletteSize;
+uniform float uSelectedId;
+uniform vec4 uSelectedColor;
+uniform float uFillOpacity;
+
+in vec2 vUv;
+out vec4 outColor;
+
+float decodeProvinceId(vec4 encoded) {
+  vec3 bytes = floor(encoded.rgb * 255.0 + 0.5);
+  return bytes.r + bytes.g * 256.0 + bytes.b * 65536.0;
+}
+
+void main() {
+  float land = texture(uLandMask, vUv).r;
+  if (land < 0.5) discard;
+
+  float provinceId = decodeProvinceId(texture(uProvinceIds, vUv));
+  if (provinceId < 0.5) discard;
+
+  float paletteIndex = provinceId - 1.0;
+  vec2 paletteUv = vec2((paletteIndex + 0.5) / uPaletteSize, 0.5);
+  vec4 color = texture(uPalette, paletteUv);
+
+  if (uSelectedId > 0.5 && abs(provinceId - uSelectedId) < 0.5) {
+    color = mix(color, uSelectedColor, 0.62);
+  }
+
+  color.a *= uFillOpacity;
+  outColor = color;
+}
+`;
