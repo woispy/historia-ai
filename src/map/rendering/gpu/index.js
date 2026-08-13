7b01610859effd5657f7b9aea8b20a75f1bb6bb5
woispy <vlknispir@gmail.com>
2026-08-13 19:06:57 +0300

    feat(map): export GPU map renderer

---
+++ src/map/rendering/gpu/index.js
@@ -0,0 +1,8 @@
+export { default as ProvinceGpuRenderer } from "./ProvinceGpuRenderer";
+export {
+  DEFAULT_HEIGHT,
+  DEFAULT_WIDTH,
+  buildProvinceRasterData,
+  encodeId,
+  parseHexColor,
+} from "./ProvinceTextureBuilder";
