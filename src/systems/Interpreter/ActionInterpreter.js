b92475a7bfe35771124871d9eee72e5610e17c19
woispy <vlknispir@gmail.com>
2026-08-15 04:28:49 +0300

    fix(runtime): make action interpreter imports ESM-safe

--- src/systems/Interpreter/ActionInterpreter.js
+++ src/systems/Interpreter/ActionInterpreter.js
@@ -1,16 +1,13 @@
-import { resolveIntent } from "./IntentResolver";
-import { resolveEntities } from "./EntityResolver";
+import { resolveIntent } from "./IntentResolver.js";
+import { resolveEntities } from "./EntityResolver.js";
 
 export function interpretAction(text) {
   const intent = resolveIntent(text);
-
   const entities = resolveEntities(text);
 
   return {
     raw: text,
-
     intent,
-
     entities,
   };
 }
