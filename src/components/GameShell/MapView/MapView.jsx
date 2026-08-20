e5b71e8e438b72eda5f11a03221b1012db40290a
woispy <vlknispir@gmail.com>
2026-08-20 17:17:57 +0300

    feat: connect city selection to map inspector

--- src/components/GameShell/MapView/MapView.jsx
+++ src/components/GameShell/MapView/MapView.jsx
@@ -1,24 +1,40 @@
 import "./MapView.css";
 
+import { getCity } from "../../../cities";
 import { getCountry } from "../../../countries";
 import { getProvince } from "../../../provinces";
+import { getAnatoliaCityMapMetadata } from "../../../map/data/AnatoliaCityAtlas.js";
 import { WorldMap } from "../../../map";
 import ProvinceInspector from "./ProvinceInspector";
+import CityInspector from "./CityInspector";
 
 function MapView({
   gameSession,
   settings,
   selectedProvinceId = null,
+  selectedCityId = null,
   onProvinceClick,
   onProvinceClose,
+  onCityClick,
+  onCityClose,
 }) {
   const provinceRepository = gameSession?.world?.repositories?.provinces;
   const countryRepository = gameSession?.world?.repositories?.countries;
+  const cityRepository = gameSession?.world?.repositories?.cities;
   const selectedProvince = provinceRepository && selectedProvinceId
     ? getProvince(provinceRepository, selectedProvinceId)
     : null;
   const selectedCountry = selectedProvince?.owner && countryRepository
     ? getCountry(countryRepository, selectedProvince.owner)
+    : null;
+  const selectedCity = cityRepository && selectedCityId
+    ? getCity(cityRepository, selectedCityId)
+    : null;
+  const selectedCityCountry = selectedCity?.owner && countryRepository
+    ? getCountry(countryRepository, selectedCity.owner)
+    : null;
+  const selectedCityMetadata = selectedCityId
+    ? getAnatoliaCityMapMetadata(selectedCityId)
     : null;
 
   return (
@@ -31,13 +47,22 @@ function MapView({
         runtime={gameSession}
         selectedProvinceId={selectedProvinceId}
         onProvinceClick={onProvinceClick}
+        onCityClick={onCityClick}
         settings={settings}
       />
-      {selectedProvince && (
+      {selectedProvince && !selectedCity && (
         <ProvinceInspector
           province={selectedProvince}
           country={selectedCountry}
           onClose={onProvinceClose}
+        />
+      )}
+      {selectedCity && (
+        <CityInspector
+          city={selectedCity}
+          country={selectedCityCountry}
+          mapMetadata={selectedCityMetadata}
+          onClose={onCityClose}
         />
       )}
     </main>
