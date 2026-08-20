df384a21dfbd0938ba635275902c6a44fcaa8f6d
woispy <vlknispir@gmail.com>
2026-08-20 10:26:49 +0300

    feat: connect province inspector to map view

--- src/components/GameShell/MapView/MapView.jsx
+++ src/components/GameShell/MapView/MapView.jsx
@@ -1,8 +1,26 @@
 import "./MapView.css";
 
+import { getCountry } from "../../../countries";
+import { getProvince } from "../../../provinces";
 import { WorldMap } from "../../../map";
+import ProvinceInspector from "./ProvinceInspector";
 
-function MapView({ gameSession, settings }) {
+function MapView({
+  gameSession,
+  settings,
+  selectedProvinceId = null,
+  onProvinceClick,
+  onProvinceClose,
+}) {
+  const provinceRepository = gameSession?.world?.repositories?.provinces;
+  const countryRepository = gameSession?.world?.repositories?.countries;
+  const selectedProvince = provinceRepository && selectedProvinceId
+    ? getProvince(provinceRepository, selectedProvinceId)
+    : null;
+  const selectedCountry = selectedProvince?.owner && countryRepository
+    ? getCountry(countryRepository, selectedProvince.owner)
+    : null;
+
   return (
     <main
       className="map-view"
@@ -11,8 +29,17 @@ function MapView({ gameSession, settings }) {
     >
       <WorldMap
         runtime={gameSession}
+        selectedProvinceId={selectedProvinceId}
+        onProvinceClick={onProvinceClick}
         settings={settings}
       />
+      {selectedProvince && (
+        <ProvinceInspector
+          province={selectedProvince}
+          country={selectedCountry}
+          onClose={onProvinceClose}
+        />
+      )}
     </main>
   );
 }
