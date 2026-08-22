c7b7455b7bdc89bc9b940e8b41bda2ecc7201bd0
woispy <vlknispir@gmail.com>
2026-08-22 04:29:03 +0300

    Keep route code splitting on React Router DOM

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import "./App.css";
 import { lazy, Suspense } from "react";
-import { BrowserRouter, Routes, Route } from "react-router";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 const MainMenu = lazy(() => import("./pages/MainMenu"));
 const NewGame = lazy(() => import("./pages/NewGame"));
