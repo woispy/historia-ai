5e999df3afd86a7580914268f27ae1e3fb4203c3
woispy <vlknispir@gmail.com>
2026-08-22 06:45:38 +0300

    refactor: export regional hydrography layer

--- src/map/components/layers/index.js
+++ src/map/components/layers/index.js
@@ -3,5 +3,6 @@ export { default as ProvinceBoundaryLayer } from "./ProvinceBoundaryLayer";
 export { default as HistoricalPoliticalRegionLayer } from "./HistoricalPoliticalRegionLayer";
 export { default as CityLayer } from "./CityLayer";
 export { default as PhysicalGeographyLayer } from "./PhysicalGeographyLayer";
+export { default as RegionalHydrographyLayer } from "./RegionalHydrographyLayer";
 export { default as WorldPhysicalLayer } from "./WorldPhysicalLayer";
 export { default as CartographyLayer } from "./CartographyLayer";
