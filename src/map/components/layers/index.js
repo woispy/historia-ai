e071c6ad1da8cb65f769d90eb49701e8c46cf94a
woispy <vlknispir@gmail.com>
2026-08-22 20:20:41 +0300

    feat: export historical viewport layer

--- src/map/components/layers/index.js
+++ src/map/components/layers/index.js
@@ -1,6 +1,7 @@
 export { default as ProvinceLayer } from "./ProvinceLayer";
 export { default as ProvinceBoundaryLayer } from "./ProvinceBoundaryLayer";
 export { default as HistoricalPoliticalRegionLayer } from "./HistoricalPoliticalRegionLayer";
+export { default as HistoricalRuntimeViewportLayer } from "./HistoricalRuntimeViewportLayer";
 export { default as CityLayer } from "./CityLayer";
 export { default as PhysicalGeographyLayer } from "./PhysicalGeographyLayer";
 export { default as RegionalHydrographyLayer } from "./RegionalHydrographyLayer";
