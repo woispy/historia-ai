e8940c98d5d9b2c8dd423586e0042d8074c752d0
woispy <vlknispir@gmail.com>
2026-08-23 07:21:07 +0300

    fix(p1): pin Nicomedia to historical cartographic anchor

--- src/map/data/AnatoliaProvinceCartographicOverrides.js
+++ src/map/data/AnatoliaProvinceCartographicOverrides.js
@@ -7,6 +7,7 @@
  */
 
 const CARTOGRAPHIC_CENTROIDS = Object.freeze({
+  "bithynia-nicomedia": [29.9169, 40.7654],
   "bithynia-nicaea": [29.73, 40.48],
   "bithynia-prusa": [29.08, 40.20],
   "bithynia-sangarios": [30.62, 40.03],
