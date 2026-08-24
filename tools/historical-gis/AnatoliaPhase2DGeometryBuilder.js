943b593dede245c7de707ab5a277d4ade0138a5c
woispy <vlknispir@gmail.com>
2026-08-24 21:41:14 +0300

    Restore Phase 2D geometry point export boundary

--- tools/historical-gis/AnatoliaPhase2DGeometryBuilder.js
+++ tools/historical-gis/AnatoliaPhase2DGeometryBuilder.js
@@ -1,5 +1,10 @@
 export {
   buildAnatoliaPhase2DAssets,
-  isAnatoliaGeometryPoint,
   isPhysicalLandPoint,
 } from "./AnatoliaPhase2DGeometryBuilderV9.js";
+
+const ANATOLIA_GEOMETRY_BBOX = [25.45, 35.72, 44.85, 42.35];
+
+export const isAnatoliaGeometryPoint = (point) => point?.length === 2
+  && point[0] >= ANATOLIA_GEOMETRY_BBOX[0] && point[0] <= ANATOLIA_GEOMETRY_BBOX[2]
+  && point[1] >= ANATOLIA_GEOMETRY_BBOX[1] && point[1] <= ANATOLIA_GEOMETRY_BBOX[3];
