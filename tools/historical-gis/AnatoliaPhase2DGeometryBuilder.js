a8575570237d89cb35146f6d09f47a06dd26017e
woispy <vlknispir@gmail.com>
2026-08-24 22:43:21 +0300

    fix: route Phase 2D geometry through V16 land recovery

--- tools/historical-gis/AnatoliaPhase2DGeometryBuilder.js
+++ tools/historical-gis/AnatoliaPhase2DGeometryBuilder.js
@@ -2,4 +2,4 @@ export {
   buildAnatoliaPhase2DAssets,
   isAnatoliaGeometryPoint,
   isPhysicalLandPoint,
-} from "./AnatoliaPhase2DGeometryBuilderV15.js";
+} from "./AnatoliaPhase2DGeometryBuilderV16.js";
