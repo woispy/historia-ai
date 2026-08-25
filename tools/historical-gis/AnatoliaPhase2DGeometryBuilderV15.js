5f48731eec9804e99abd9ce60ea7f0b5d3ef5713
woispy <vlknispir@gmail.com>
2026-08-25 16:58:25 +0300

    fix: remove unused physical-land partition import

--- tools/historical-gis/AnatoliaPhase2DGeometryBuilderV15.js
+++ tools/historical-gis/AnatoliaPhase2DGeometryBuilderV15.js
@@ -3,7 +3,7 @@ import { ANATOLIA_PHYSICAL_ATLAS } from "../../src/map/data/AnatoliaPhysicalAtla
 import { ANATOLIA_PROVINCE_METADATA } from "../../src/map/data/AnatoliaProvinceMetadata.js";
 import { ANATOLIA_1300_PROVINCE_GEOMETRY_MANIFEST, ANATOLIA_1300_PROVINCE_GEOMETRY_KEYS } from "../../src/map/data/Anatolia1300ProvinceGeometryManifest.js";
 import { ANATOLIA_PROVINCE_REFINEMENTS, ANATOLIA_STRATEGIC_PASSES, ANATOLIA_RIVER_CROSSINGS } from "../../src/map/data/AnatoliaProvinceRefinement.js";
-import { PHYSICAL_LAND_POLYGONS, isPhysicalLandPoint, isPhysicalGeometryBoundaryPoint, isFinalPhysicalGeometryBoundaryPoint, resolveGeometryAnchor, resolvePhysicalGeometryBoundaryPoint } from "./recovery/physical-land-authority.mjs";
+import { isPhysicalLandPoint, isPhysicalGeometryBoundaryPoint, isFinalPhysicalGeometryBoundaryPoint, resolveGeometryAnchor, resolvePhysicalGeometryBoundaryPoint } from "./recovery/physical-land-authority.mjs";
 const BBOX=[25.45,35.72,44.85,42.35],EPS=1e-10,MIN_AREA=.00005,SAMPLE_STEP=.06,MAX_AREA_RATIO=4.2,MAX_WEIGHT_ITERATIONS=24,MAX_WEIGHT_STEP=4,FEATURE_WEIGHT_STEP=.01;
 const PARTITION_LAND_POLYGONS=ANATOLIA_PHYSICAL_ATLAS.landPolygons.filter(polygon=>polygon.length>=3&&Math.abs((polygon.reduce((sum,point,index)=>{const next=polygon[(index+1)%polygon.length];return sum+point[0]*next[1]-next[0]*point[1];},0))/2)>=MIN_AREA);
 const rawAnchor=item=>ANATOLIA_PROVINCE_REFINEMENTS[item.id]?.anchor??item.centroid;
