a5f622621cd0f8b2611f2df2ec9184818fb1ef01
woispy <vlknispir@gmail.com>
2026-08-29 04:13:11 +0300

    Route Phase 2D V8 compatibility layer to V12 core

--- tools/historical-gis/AnatoliaPhase2DGeometryBuilderV8.js
+++ tools/historical-gis/AnatoliaPhase2DGeometryBuilderV8.js
@@ -1,14 +1,14 @@
 import {
-  buildAnatoliaPhase2DAssets as buildV11Assets,
+  buildAnatoliaPhase2DAssets as buildV12Assets,
   isAnatoliaGeometryPoint,
   isPhysicalLandPoint,
   isPhysicalLandPointLegacy,
-} from "./AnatoliaPhase2DGeometryBuilderV11.js";
+} from "./AnatoliaPhase2DGeometryBuilderV12.js";
 
 export { isAnatoliaGeometryPoint, isPhysicalLandPoint, isPhysicalLandPointLegacy };
 
 export function buildAnatoliaPhase2DAssets() {
-  const assets = buildV11Assets();
+  const assets = buildV12Assets();
   const generator = "Historia AI Phase 2D Geometry Builder";
   for (const asset of [...assets.provinces, ...assets.geometries]) {
     if (asset.header) asset.header.generator = generator;
