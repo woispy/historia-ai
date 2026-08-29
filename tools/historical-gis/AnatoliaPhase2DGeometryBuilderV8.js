7aa7e41dcbc98fbb2131f625c05cb945b0512686
woispy <vlknispir@gmail.com>
2026-08-29 04:08:40 +0300

    Normalize Phase 2D public V8 asset header contract

--- tools/historical-gis/AnatoliaPhase2DGeometryBuilderV8.js
+++ tools/historical-gis/AnatoliaPhase2DGeometryBuilderV8.js
@@ -1,6 +1,17 @@
-export {
-  buildAnatoliaPhase2DAssets,
+import {
+  buildAnatoliaPhase2DAssets as buildV11Assets,
   isAnatoliaGeometryPoint,
   isPhysicalLandPoint,
   isPhysicalLandPointLegacy,
 } from "./AnatoliaPhase2DGeometryBuilderV11.js";
+
+export { isAnatoliaGeometryPoint, isPhysicalLandPoint, isPhysicalLandPointLegacy };
+
+export function buildAnatoliaPhase2DAssets() {
+  const assets = buildV11Assets();
+  const generator = "Historia AI Phase 2D Geometry Builder";
+  for (const asset of [...assets.provinces, ...assets.geometries]) {
+    if (asset.header) asset.header.generator = generator;
+  }
+  return assets;
+}
