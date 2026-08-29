9c7b4fea96f0896eebba03d88ab736a764d98e41
woispy <vlknispir@gmail.com>
2026-08-29 03:59:02 +0300

    Route Phase 2D wrapper to V9 core

--- tools/historical-gis/AnatoliaPhase2DGeometryBuilder.js
+++ tools/historical-gis/AnatoliaPhase2DGeometryBuilder.js
@@ -2,4 +2,4 @@ export {
   buildAnatoliaPhase2DAssets,
   isAnatoliaGeometryPoint,
   isPhysicalLandPoint,
-} from "./AnatoliaPhase2DGeometryBuilderV8.js";
+} from "./AnatoliaPhase2DGeometryBuilderV9.js";
