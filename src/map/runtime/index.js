0b7cc9f852f62e172d01a3af24920e223402a38e
woispy <vlknispir@gmail.com>
2026-08-31 14:31:47 +0300

    feat: export GPU-native map engine

--- src/map/runtime/index.js
+++ src/map/runtime/index.js
@@ -1,3 +1,4 @@
 export { ProvinceSoA } from "./ProvinceSoA.js";
 export { QuadtreeIndex, buildSpatialItems } from "./SpatialIndex.js";
 export { MapCameraRig } from "./MapCameraRig.js";
+export { GpuMapEngine } from "./GpuMapEngine.js";
