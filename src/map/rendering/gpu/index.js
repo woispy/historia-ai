cc6c879bc3248d788943b04e4468de04ef0838c3
woispy <vlknispir@gmail.com>
2026-09-01 12:19:00 +0300

    refactor: export binary GPU renderer

--- src/map/rendering/gpu/index.js
+++ src/map/rendering/gpu/index.js
@@ -1,3 +1,3 @@
 export { default as GpuMapRenderer } from "./GpuMapRenderer.js";
+export { default as BinaryMapRenderer } from "./BinaryMapRenderer.js";
 export { default as WebGPUMapRenderer } from "./WebGPUMapRenderer.js";
-export { buildGpuAssetBridge } from "./MapAssetBridge.js";
