2f3fcc23c404547f367cb9ce01fb07a4c199945f
woispy <vlknispir@gmail.com>
2026-09-01 12:46:09 +0300

    H2: expose browser mapbin loader

--- src/map/runtime/index.js
+++ src/map/runtime/index.js
@@ -1,6 +1,6 @@
 export { ProvinceSoA } from "./ProvinceSoA.js";
 export { BinaryMapAssetSource } from "./BinaryMapAssetSource.js";
-export { buildMapBinFromProvinces } from "./BinaryMapAssetBuilder.js";
+export { loadMapBin, DEFAULT_MAPBIN_URL } from "./MapBinLoader.js";
 export { QuadtreeIndex, buildSpatialItems } from "./SpatialIndex.js";
 export { MapCameraRig } from "./MapCameraRig.js";
 export { MapRuntimeController } from "./MapRuntimeController.js";
