5b300ccf486eaa2485d67efcf93fed526c09d71b
woispy <vlknispir@gmail.com>
2026-09-01 12:19:04 +0300

    refactor: export binary map runtime assets

--- src/map/runtime/index.js
+++ src/map/runtime/index.js
@@ -1,3 +1,6 @@
 export { ProvinceSoA } from "./ProvinceSoA.js";
+export { BinaryMapAssetSource } from "./BinaryMapAssetSource.js";
+export { buildMapBinFromProvinces } from "./BinaryMapAssetBuilder.js";
 export { QuadtreeIndex, buildSpatialItems } from "./SpatialIndex.js";
 export { MapCameraRig } from "./MapCameraRig.js";
+export { MapRuntimeController } from "./MapRuntimeController.js";
