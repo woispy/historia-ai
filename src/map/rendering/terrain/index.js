8676c3c3fa1b30ae23245d1e007a5af51eb62730
woispy <vlknispir@gmail.com>
2026-09-02 23:45:08 +0300

    Phase E terrain: restore consolidated terrain module exports

---
+++ src/map/rendering/terrain/index.js
@@ -0,0 +1,7 @@
+export * from "./TerrainGeometry.js";
+export * from "./TerrainLod.js";
+export * from "./TerrainSeams.js";
+export * from "./TerrainStreaming.js";
+export * from "./TerrainTile.js";
+export * from "./TerrainTileManifest.js";
+export * from "./TerrainTopology.js";
