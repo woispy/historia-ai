4a4d4b726f89a4ef8c7ae3da8ce4ed51ef3c5fea
woispy <vlknispir@gmail.com>
2026-09-05 14:47:07 +0300

    Restore Node path import in historical GIS validator

--- tools/historical-gis/cli/validate-1300.js
+++ tools/historical-gis/cli/validate-1300.js
@@ -1,5 +1,5 @@
 import fs from "node:fs/promises";
-import path from "path";
+import path from "node:path";
 import { fileURLToPath } from "node:url";
 import { importHistoricalGeoJson } from "../HistoricalGeometryImporter.js";
 
